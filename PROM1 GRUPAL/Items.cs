ee9b48bec275b60ce5cb0a32115325799dcb5dd9
AdrianPacheco <teyliugmax@gmail.com>
2024-09-16 18:35:39 -0500

    ItemsAdd

---
+++ PROM1 GRUPAL/Items.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PROM1_GRUPAL
+{
+    public class Items
+    {
+        private string name;
+        private string description;
+
+        public Items(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            private set { description = value; }
+        }
+
+        private static List<Items> itemList = new List<Items>();
+
+        public static List<Items> GetItems()
+        {
+            return new List<Items>(itemList); 
+        }
+        public static void AddItem(Items item)
+        {
+            itemList.Add(item);
+        }
+    }
+}
+
