7e1429eb074df71b4b347aa0602031d3231884b4
Laboratorio J601 TLS-JP <labj601-tlsjp@campustls.local>
2024-09-16 19:23:54 -0500

    Enemy class

---
+++ PROM1 GRUPAL/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROM1_GRUPAL
+{
+    internal class Enemy
+    {
+        private string enemy;
+        private int health;
+        private int damage;
+        public Enemy(string enemy, int health, int damage)
+        {
+            this.enemy = enemy;
+            this.health = health;
+            this.damage = damage;
+        }
+        public void Damage(int damage)
+        {
+            health += damage;
+        }
+        public void GetDamage()
+        {
+
+        }
+    }
+}
